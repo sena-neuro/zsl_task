97168383d8f3b27e029fcc93b613c53ed67b471b
valkyrie7 <iamotley7@gmail.com>
2017-08-20 17:37:11 +0300

    Debugged, batches are implemented

--- load_data_test.py
+++ load_data_test.py
@@ -1,4 +1,6 @@
 import h5py
+import tensorflow as tf
+import numpy as np
 
 def batch(iterable, n=1):
     l = len(iterable)
@@ -10,15 +12,24 @@ def get_training_data(filename):
     dset_list = {}
     for dsetname, dset in f.iteritems():
         if dsetname == 'Xtr' or dsetname == 'Str_gt' or dsetname == 'Ltr_oh':
-            dset_list[dsetname] = dset[:]
+            dset_list[dsetname] = np.transpose(dset[:])     # ????????
     return dset_list
 
 
 dataset = get_training_data('gbu_CUB_data.mat')
 
-print dataset
+import numpy as np
+"""
+ here we need to batch only Ltr_oh and Xtr 
+"""
+def next_batch(num, data, labels):
+    """
+    Return a total of `num` random samples and labels.
+    """
+    idx = np.arange(0, len(data[0]))
+    np.random.shuffle(idx)
+    idx = idx[:num]
+    data_shuffle = [data[i][:] for i in idx]
+    labels_shuffle = [labels[i][:] for i in idx]
 
-def getBAATCHED(key):
-    for x in batch(dataset[key], 2):
-        print x
-        print "batch end"
+    return np.asarray(data_shuffle), np.asarray(labels_shuffle)
--- zsl_task.py
+++ zsl_task.py
@@ -2,19 +2,19 @@ import tensorflow as tf
 import load_data_test as ld
 import numpy as np
 
-def main(_):
+def main():
     # Import data
     dataset = ld.get_training_data('gbu_CUB_data.mat')
 
     # Create the model
-    m = tf.placeholder(tf.float32)
+    m = 100
     X_tr = tf.placeholder(tf.float32, [m, 2048])
-    W = tf.Variable(tf.float32, [2048, 100])  # init with Xavier
+    W = tf.Variable(tf.zeros([2048, 312]))  # init with Xavier
 
     # Define loss
     S_guess = tf.matmul(X_tr, W)          # Attributes [None, 100]
     S_gt = tf.placeholder(tf.float32, [100, 312])
-    S_corr = tf.matmul(S_guess, S_gt)
+    S_corr = tf.matmul(S_guess, tf.transpose(S_gt))
     L_tr_oh = tf.placeholder(tf.float32, [m, 100])
     L_tr_temp = tf.multiply(L_tr_oh, S_corr)
     L_tr = tf.reduce_sum(L_tr_temp, 1, keep_dims=True)
@@ -28,23 +28,18 @@ def main(_):
     global_step = tf.Variable(0, name='global_step', trainable=False)
     train_op = optimizer.minimize(loss, global_step=global_step)
 
-    with tf.InteractiveSession() as sess:
+    with tf.Session() as sess:
         tf.global_variables_initializer().run()
 
-        X_tr = dataset['Xtr']
-        S_gt = dataset['Str_gt']
-        L_tr_oh = dataset['Ltr_oh']
-
-        # TODO: feed_dict
         feed_dict = {}
         for _ in range(1000):
-            # TODO: import data
-            batch_X_tr = ld.getBAATCHED('Xtr')
-            batch_S_gt = ld.getBAATCHED('Str_gt')
-            batch_L_tr_oh = ld.getBAATCHED('Ltr_oh')
-            sess.run(train_step, feed_dict={X_tr: batch_X_tr:, S_gt: batch_S_gt:, L_tr_oh: batch_L_tr_oh})
+            batch_X_tr, batch_L_tr_oh = ld.next_batch(100, dataset['Xtr'], dataset['Ltr_oh'])
+            Str_gt = dataset['Str_gt']
 
+            sess.run(train_op, feed_dict={X_tr: batch_X_tr, S_gt: Str_gt, L_tr_oh: batch_L_tr_oh})
 
+    # TODO: Compute accuracy
+        print(sess.run(W))
 
-
+main()
 
