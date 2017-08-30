d28a380c98bd4ca609b2014753f7bb30b34bedec
Mert Bulent Sariyildiz <mbsariyildiz@gmail.com>
2017-08-30 16:59:05 +0300

    Feedback

--- zsl_task.py
+++ zsl_task.py
@@ -1,3 +1,4 @@
+import os
 import tensorflow as tf
 import utils as ut
 import numpy as np
@@ -9,62 +10,96 @@ def calculate_accuracy(preds, labels):
     return accuracy
 
 def main():
+
     # Import data
-    dataset = ut.get_data('gbu_CUB_data.mat')
+    # relative dataset paths should be supported
+    if os.uname()[1] == 'zen': dset_path = "/home/mbs/Datasets/zsl/gbu_CUB_data.mat"
+    else: dset_path = "gbu_CUB_data.mat"
+    dset = ut.get_data(dset_path)
 
     # Create the model
-    X = tf.placeholder(tf.float32, [None, 2048])
+    # Bulent: Its good habit to separate placeholders from the rest of the model
+    #         Since you can always watch out for feeds.
+    #         Also consider naming placeholders, it helps solving certain bugs.
+    X = tf.placeholder(tf.float32, [None, 2048], name='features')
+    L_oh = tf.placeholder(tf.float32, [None, None], name='one-hot_labels')                         # Labels one hot encoded
+    S_gt = tf.placeholder(tf.float32, [None, 312], name='class_embeddings')                          # Ground Truth for test attributes
+    reg_constant = tf.placeholder(tf.float32, [], name='regularization_constant')
+
     W = tf.Variable(tf.zeros([2048, 312]))
 
     # Define loss
     S_guess = tf.matmul(X, W)                                               # Attributes [None, 312]
-    S_gt = tf.placeholder(tf.float32, [None, 312])                          # Ground Truth for test attributes
     S_corr = tf.matmul(S_guess, tf.transpose(S_gt))
-    L_oh = tf.placeholder(tf.float32, [None, None])                         # Labels one hot encoded
     Corr_oh = tf.reduce_sum(tf.multiply(L_oh, S_corr), 1, keep_dims=True)   # Correlations one hot encoded
     L_oh_dual = tf.ones_like(L_oh) - L_oh
     loss_matrix = tf.add(tf.subtract(S_corr, Corr_oh), L_oh_dual)
     max_scores = tf.maximum(loss_matrix, tf.zeros_like(loss_matrix))
-    unregularized_loss = tf.reduce_mean(tf.reduce_sum(max_scores))
+    unregularized_loss = tf.reduce_mean(tf.reduce_sum(max_scores, 1))
 
     # Regularization
-    reg_constant = tf.placeholder(tf.float32)
+    # Bulent: typical regularization constant ranges between [1e-3, 1e-6], 
+    #         it should be tuned on validation set
     l2_loss = reg_constant * tf.nn.l2_loss(W)
 
     # Loss
-    loss = tf.add(unregularized_loss,l2_loss)
+    loss = tf.add(unregularized_loss, l2_loss)
 
     # Define optimizer
+    # Bulent: Ive never seen a learning rate that decreases too rapid,
+    #         Tune deceleration on validation set
     global_step = tf.Variable(0, name='global_step', trainable=False)
     starter_learning_rate = 0.001
     learning_rate = tf.train.exponential_decay(starter_learning_rate, global_step,
-                                               500, 0.96, staircase=True)
+                                               500, 0.996, staircase=True)
 
     optimizer = tf.train.AdamOptimizer(learning_rate)
     train_op = optimizer.minimize(loss, global_step=global_step)
 
     accuracy = calculate_accuracy(S_corr, L_oh)
 
+    # Bulent: Measuring loss of a batch of samples right after processing them does not
+    #         reflect the truth. Its good practice to evaluate the model after predefined time
+    #         intervals, i.e. epochs.
+    n_iters = 100000
+
     with tf.Session() as sess:
         tf.global_variables_initializer().run()
 
-        for i in range(10000):
-            batch_X_tr, batch_L_tr_oh = ut.next_batch(100, dataset['Xtr'], dataset['Ltr_oh'])
+        for it in xrange(1, n_iters + 1):
+            batch_X_tr, batch_L_tr_oh = ut.next_batch(100, dset['Xtr'], dset['Ltr_oh'])
+            sess.run(
+                train_op,
+                {
+                    X: batch_X_tr, 
+                    L_oh: batch_L_tr_oh, 
+                    S_gt: dset['Str_gt'],
+                    reg_constant: 1e-4
+                }
+            )
 
-            # to get loss i changed below line
-            _, loss_cur, train_acc = sess.run([train_op, loss, accuracy, l2_loss],
-                                              feed_dict={X: batch_X_tr, S_gt: dataset['Str_gt'],
-                                                         L_oh: batch_L_tr_oh, reg_constant: 3})
+            # evaluate model once in a while
+            if it % 1000 == 0:
+                tr_acc, _unregLoss = sess.run(
+                    [accuracy, unregularized_loss],
+                    {
+                        X: dset['Xtr'],
+                        L_oh: dset['Ltr_oh'],
+                        S_gt: dset['Str_gt']
+                    }
+                )
 
-            val_acc = accuracy.eval(
-                feed_dict={X: dataset['Xva'], L_oh: dataset['Lva_oh'], S_gt: dataset['Sva_gt']})
-            if i % 10 == 0:
-                print 'Batch no = {0}, current loss = {1}, Current training accuracy = {2},' \
-                      ' current validation accuracy = {3}'.format(i,loss_cur,train_acc,val_acc)
+                va_acc = sess.run(
+                    accuracy,
+                    {
+                        X: dset['Xva'],
+                        L_oh: dset['Lva_oh'],
+                        S_gt: dset['Sva_gt']
+                    }
+                )
 
-        fin_val_acc = accuracy.eval(
-            feed_dict={X: dataset['Xva'], L_oh: dataset['Lva_oh'], S_gt: dataset['Sva_gt']})
+                print 'Iter: {0:05}, loss = {1:09.5f}, acc_tr = {2:0.4f}, acc_va = {2:0.4f}' .format(
+                    it, _unregLoss, tr_acc, va_acc)
 
-        print fin_val_acc
 main()
 
